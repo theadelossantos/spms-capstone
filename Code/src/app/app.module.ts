17188d0011ace1dc7d27efeed5693940d49fb91c
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2023-08-09 18:21:55 +0800

    upddate

--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './loginpage/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
